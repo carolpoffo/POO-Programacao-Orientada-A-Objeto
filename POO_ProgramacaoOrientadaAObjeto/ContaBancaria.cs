6cdbc83692818182114f8cca38422ee5b599aa88
carolpoffo <carolpoffo98@gmail.com>
2025-03-14 09:42:44 -0300

    first commit

---
+++ POO_ProgramacaoOrientadaAObjeto/ContaBancaria.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_ProgramacaoOrientadaAObjeto
+{
+    class ContaBancaria
+    {
+        //atributos privados
+        private int numeroConta;
+        private string titular;
+        private double saldo;
+
+        public ContaBancaria(int numeroConta, string titular, double saldoInicial)
+        {
+            this.numeroConta = numeroConta;
+            this.titular = titular;
+            this.saldo = saldoInicial;
+        }
+        //acessar os atributos
+        public int getNumeroConta()
+        {
+            return numeroConta;
+        }
+        public string getTitular()
+        {
+            return titular;
+        }
+        public double getSaldo()
+        {
+            return saldo;
+        }
+        public void Depositar(double valor)
+        {
+            if (valor > 0)
+            {
+                saldo += valor;
+            }
+        }
+        public bool Sacar(double valor)
+        {
+            if (valor > 0 && saldo >= valor) //pode sacar se valor for maior que zero e tiver saldo
+            {
+                saldo-= valor;
+                return true;
+            }
+            return false;
+        }
+    }
+}
